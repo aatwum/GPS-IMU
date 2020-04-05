7ed3f21b248996891eb1c21bb9519d0bc4782cad
Ignat Penshin <ignat.penshin51@gmail.com>
2020-04-05 19:34:21 +0300

    45sec fills and full print of pd

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ import os
 
 #args: (nmea_file, imu_file, near_time_NMEA, near_time_IMU, median_time):
 time_new = t_dict('test/050420/test_2/nmea_moded_for_tests.NMEA', 
-                                                    'test/050420/test_2/imu_2.csv', 20, 0.2, 2)
-
+                                                    'test/050420/test_2/imu_2.csv', 45, 0.2, 2)
+pd.set_option("display.max_rows", None, "display.max_columns", None)
 print(pd.DataFrame(time_new))                                                   
 
 #args: (nmea_file, imu_file, near_time_NMEA, near_time_IMU, median_time):
